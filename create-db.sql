184c058036cba1a9a8201a36864649e0ab284ce9
Andreas Huber <ahd6974-githubcom@outlook.com>
2018-02-12 15:05:31 +0100

    Test DB dump restore as part of the backup

--- create-db.sql
+++ create-db.sql
@@ -1,4 +1,6 @@
 create database owncloud;
 create user owncloud@localhost identified by '12345';
 grant all privileges on owncloud.* to owncloud@localhost identified by '12345';
+-- It is legal to grant privileges to non-existing databases. This DB will be used to test a DB backup
+grant all privileges on owncloud_restore_test.* to owncloud@localhost identified by '12345';
 flush privileges;
